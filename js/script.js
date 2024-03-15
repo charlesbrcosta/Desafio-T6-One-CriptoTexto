db9b205edda92a3f07cb34d53dd981113600cef6
Charles Bruno <charlesbrcosta@gmail.com>
2024-03-15 20:54:01 -0300

    adicionar  about.html e o contact.html. Ajuste no script.js, styles.css glocal.css

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ closeBtn.addEventListener('click', () => {
 
 //Adiciona uma escuta para alternar de DarkMode para LightMode
 darkMode.addEventListener('click', () => {
+    console.log('DarkeMode clicked');
     document.body.classList.toggle('dark-mode-variables');
     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
@@ -37,8 +38,6 @@ window.addEventListener('resize', () => {
 // ---------------------------------------------------- //
 
 //Adiciona um foco na textArea quando selecionada
-
-
 // selectiona o textarea
 const textarea = document.querySelectorAll('textarea');
 
@@ -57,5 +56,53 @@ textarea.forEach( textarea => {
         container.classList.remove('focus');
     });
 }); 
+
+
+//Adiciona o estilo no sidebar ao clicar no link e redireciona a página.
+document.addEventListener('DOMContentLoaded', function() {
+    // Se houver um link ativo no localStorage, aplica a classe active
+    let activeLink = localStorage.getItem('activeLink');
+    if (activeLink) {
+        let link = document.querySelector('[data-target="' + activeLink + '"]');
+        if (link) {
+            link.classList.add('active');
+        }
+    } else {
+        //Se não houver link ativo, esse será o link padrão.
+        let defaultlink = document.querySelector('[data-target="index"]');
+        if(defaultlink){
+            defaultlink.classList.add('active');
+            localStorage.setItem('activeLink', 'index'); //Salva o link padrão index.html no localstorage
+        }
+    }
+
+    // Adiciona evento de clique aos links
+    let links = document.querySelectorAll('.sidebar .nav-link');
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            let target = this.getAttribute('data-target');
+            setActive(target);
+            window.location.href = target + '.html'; //redireciona para a página correspondente
+        });
+    });
+});
+
+function setActive(target) {
+    // Remove a classe 'active' de todos os links
+    let links = document.querySelectorAll('.sidebar .nav-link');
+    links.forEach(function(link) {
+        link.classList.remove('active');
+    });
+
+    // Adiciona a classe 'active' apenas ao link clicado
+    let activeLink = document.querySelector('[data-target="' + target + '"]');
+    if (activeLink) {
+        activeLink.classList.add('active');
+        // Salva o link ativo no localStorage
+        localStorage.setItem('activeLink', target);
+    }
+}
+
 
 
